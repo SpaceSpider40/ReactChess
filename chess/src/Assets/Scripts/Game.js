5d03d0d54556a41e0a972e998b44072cd9a4fc04
SpaceSpider40 <60621940+SpaceSpider40@users.noreply.github.com>
2021-12-24 21:15:07 +0100

    Update Game.js

--- chess/src/Assets/Scripts/Game.js
+++ chess/src/Assets/Scripts/Game.js
@@ -1,11 +1,24 @@
 import React from "react";
 
 class Game extends React.Component{
+<<<<<<< Updated upstream
     constructor(props){
         super(props);
     
     }
    
+=======
+    constructor(props) {
+        super(props);   
+
+        this.getPlayerNames = this.getPlayerNames.bind(this);
+    }
+
+    getPlayerNames(p1,p2){
+        console.log(p1,p2);
+    }
+
+>>>>>>> Stashed changes
     render(){
         return('Game started')
     }   
