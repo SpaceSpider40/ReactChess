884bceaffb5adb3a0fcdf77bfa5d3bdec6178861
Sewecz <94013389+Sewecz@users.noreply.github.com>
2021-12-27 20:21:01 +0100

    Update GenerateBoard.js

---
+++ chess/src/Assets/Scripts/GenerateBoard.js
@@ -0,0 +1,97 @@
+import React from "react";
+
+class Board extends React.Component{
+    constructor(props){
+        super(props);
+
+    }
+    generateboard(props){
+        const board =<div id="board"><grid>
+            <item id="8a"></item>
+            <item id="8b"></item>
+            <item id="8c"></item>
+            <item id="8d"></item>
+            <item id="8e"></item>
+            <item id="8f"></item>
+            <item id="8g"></item>
+            <item id="8h"></item>
+        </grid>
+        <grid>
+            <item id="7a"></item>
+            <item id="7b"></item>
+            <item id="7c"></item>
+            <item id="7d"></item>
+            <item id="7e"></item>
+            <item id="7f"></item>
+            <item id="7g"></item>
+            <item id="7h"></item>
+        </grid>
+
+        <grid>
+            <item id="6a"></item>
+            <item id="6b"></item>
+            <item id="6c"></item>
+            <item id="6d"></item>
+            <item id="6e"></item>
+            <item id="6f"></item>
+            <item id="6g"></item>
+            <item id="6h"></item>
+        </grid>
+        <grid>
+            <item id="5a"></item>
+            <item id="5b"></item>
+            <item id="5c"></item>
+            <item id="5d"></item>
+            <item id="5e"></item>
+            <item id="5f"></item>
+            <item id="5g"></item>
+            <item id="5h"></item>
+        </grid>
+        <grid>
+            <item id="4a"></item>
+            <item id="4b"></item>
+            <item id="4c"></item>
+            <item id="4d"></item>
+            <item id="4e"></item>
+            <item id="4f"></item>
+            <item id="4g"></item>
+            <item id="4h"></item>
+        </grid>
+        <grid>
+            <item id="3a"></item>
+            <item id="3b"></item>
+            <item id="3c"></item>
+            <item id="3d"></item>
+            <item id="3e"></item>
+            <item id="3f"></item>
+            <item id="3g"></item>
+            <item id="3h"></item>
+        </grid>
+        <grid>
+            <item id="2a"></item>
+            <item id="2b"></item>
+            <item id="2c"></item>
+            <item id="2d"></item>
+            <item id="2e"></item>
+            <item id="2f"></item>
+            <item id="2g"></item>
+            <item id="2h"></item>
+        </grid>
+        <grid>
+            <item id="1a"></item>
+            <item id="1b"></item>
+            <item id="1c"></item>
+            <item id="1d"></item>
+            <item id="1e"></item>
+            <item id="1f"></item>
+            <item id="1g"></item>
+            <item id="1h"></item>
+        </grid>
+         </div>;
+    }
+
+render(){
+
+
+}
+}
