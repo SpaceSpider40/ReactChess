c23005344737a5e8d10e2966a434ed78a8de9644
Sewecz <94013389+Sewecz@users.noreply.github.com>
2022-01-16 02:23:34 +0100

    main

--- chess/src/Assets/Scripts/Timer.js
+++ chess/src/Assets/Scripts/Timer.js
@@ -7,53 +7,43 @@ class Timer extends React.Component{
         this.state = {
             seconds:  0,
             times: []
-        }
+        };
         this.endturn= this.endturn.bind(this);
-        this.table =this.table.bind(this);
+
     }
 
     componentDidMount(){
         this.timerID = setInterval(()=>this.tick(),1000)
     }
-
+   s
     tick(){
         this.setState({
             seconds:this.state.seconds+1
         })
     }
     endturn(){
-        clearInterval(this.timerID);
-       let num = this.state.seconds;
-       console.log(num);
-       
-        this.setState(previousState => ({
-            times: [previousState.times,num ]
-        }));
-              
+        clearInterval(this.timerID);  
+        this.state.times.push(this.state.seconds);
               this.setState({
                   seconds: 0
               })
-              this.clock =setInterval(()=>this.tick(),1000);
-             
+              this.timerID =setInterval(()=>this.tick(),1000);
+              console.log(this.state.times);
             }
-        table(){
-            
-            return(<ol>
-            {this.times.map((time)=>(
-                <li>{time}</li>
-            ))}
-        </ol>
-            );
-            
-        }
+     
     render(){
-        return(<div>
-            <h2>Czas tury</h2>
-            <p> 
-                {this.state.seconds}
-            </p>
+        return(<div className={style.timer}>
+            <h2>Czas tury {this.state.seconds}</h2>
+            
             <button className={style.endturn} onClick={this.endturn}>Endturn</button>
-         <p>{this.times}</p>
+                 <ul>Czasy tur
+                     
+                 {this.state.times.slice(-5).map((time)=>(
+                   <li key={time.id}>{time}</li>
+                   
+                ))}
+                </ul>
+           
         </div>);
     }
 }
